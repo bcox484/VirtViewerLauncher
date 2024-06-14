ce9292a31e8748ef94d7be52f6d23eeda4802cd0
bcox484 <bcox484@outlook.com>
2024-06-14 00:22:30 -0600

    less dumb

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cp virt-viewer-launcher.py /usr/local/bin/virt-viewer-launcher
-chmod 755 /usr/local/bin/virt-viewer-launcher
-
 if [ "$1" == "uninstall" ]; then
     rm /usr/local/bin/virt-viewer-launcher
+else
+    cp virt-viewer-launcher.py /usr/local/bin/virt-viewer-launcher
+    chmod 755 /usr/local/bin/virt-viewer-launcher
 fi
+
